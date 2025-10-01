63c6e4ead273aa509c99b29fe6d50871b604226d
kingro27 <kumarvasu2006@gmail.com>
2025-10-01 09:37:03 +0545

    moved nvf-conf.nix and tlp.nix to the parent directory for testing

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,8 @@
   imports =
     [ 
       ./hardware-configuration.nix
+      ./nvf-conf.nix
+      ./tlp.nix
     ];
 
   # Bootloader.
@@ -41,22 +43,22 @@
 
   services.flatpak.enable = true;
 
-  programs.nvf = {
-    enable = true;
-    settings = {
-        vim = {
-                theme = {
-                        enable = true;
-                        name = "gruvbox";
-                        style = "dark";
-                };
-
-                statusline.lualine.enable = true;
-                telescope.enable = true;
-                autocomplete.nvim-cmp.enable = true;
-        };
-      };
-    };
+#  programs.nvf = {
+#    enable = true;
+#    settings = {
+#        vim = {
+#                theme = {
+#                        enable = true;
+#                        name = "gruvbox";
+#                        style = "dark";
+#                };
+#
+#                statusline.lualine.enable = true;
+#                telescope.enable = true;
+#                autocomplete.nvim-cmp.enable = true;
+#        };
+#      };
+#    };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.doctor = {
@@ -76,33 +78,33 @@
 	};
   };
 
-  services.tlp = {
-  	enable = true;
-	settings = {
-		# Change CPU energy/performance policy
-		CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-		CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-
-		# Enable the platform profiler
-		PLATFORM_PROFILE_ON_AC = "performance";
-		PLATFORM_PROFILE_ON_BAT = "low-power";
-
-		# Disable turbo boost
-		CPU_BOOST_ON_AC = 1;
-		CPU_BOOST_ON_BAT = 0;
-
-		CPU_HWP_DYN_BOOST_ON_AC = 1;
-		CPU_HWP_DYN_BOOST_ON_BAT = 0;
-
-		# Reduce power consumption / fan noice on AC power
-		RUNTIME_PM_ON_AC = "auto";
-		RUNTIME_PM_ON_BAT = "auto";
-
-		# Limit power consumption under high CPU load
-		CPU_MAX_PERF_ON_AC = 100;
-		CPU_MAX_PERF_ON_BAT = 25;
-	};
-  };
+#  services.tlp = {
+#  	enable = true;
+#	settings = {
+#		# Change CPU energy/performance policy
+#		CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+#		CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+#
+#		# Enable the platform profiler
+#		PLATFORM_PROFILE_ON_AC = "performance";
+#		PLATFORM_PROFILE_ON_BAT = "low-power";
+#
+#		# Disable turbo boost
+#		CPU_BOOST_ON_AC = 1;
+#		CPU_BOOST_ON_BAT = 0;
+#
+#		CPU_HWP_DYN_BOOST_ON_AC = 1;
+#		CPU_HWP_DYN_BOOST_ON_BAT = 0;
+#
+#		# Reduce power consumption / fan noice on AC power
+#		RUNTIME_PM_ON_AC = "auto";
+#		RUNTIME_PM_ON_BAT = "auto";
+#
+#		# Limit power consumption under high CPU load
+#		CPU_MAX_PERF_ON_AC = 100;
+#		CPU_MAX_PERF_ON_BAT = 25;
+#	};
+#  };
 
 
   programs.dconf.enable = true;
